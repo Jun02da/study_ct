d6baa9c0fe73758ca8ccf80d87d8e2b6c7c93e91
Jun02da <124124840+Jun02da@users.noreply.github.com>
2023-06-20 15:48:15 +0900

    [Bronze V] Title: A/B, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = bf.readLine().split(" ");
+		int num1 = Integer.parseInt(str[0]);
+		int num2 = Integer.parseInt(str[1]);
+		
+		double result = (double)num1/num2;
+		System.out.println(result);
+	}
+}
