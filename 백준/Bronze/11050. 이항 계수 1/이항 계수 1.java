291339ff50c1a7f33fcb520e3a5ae87abe06eac5
Jun02da <124124840+Jun02da@users.noreply.github.com>
2023-07-13 10:04:44 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int N = Integer.parseInt(str[0]);
+        int K = Integer.parseInt(str[1]);
+        int result = bino_coef(N, K);
+        System.out.println(result);
+    }
+
+    static int bino_coef(int n, int k) {
+        if (k > n) {
+            return 0;
+        }
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return bino_coef(n - 1, k - 1) + bino_coef(n - 1, k);
+    }
+}
