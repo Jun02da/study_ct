24d7810327d957993bb0cc31358e696c5e154876
Jun02da <124124840+Jun02da@users.noreply.github.com>
2023-07-14 08:56:07 +0900

    [Silver V] Title: 나이순 정렬, Time: 516 ms, Memory: 51148 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,56 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-
-class Member {
-    int age;
-    String name;
-
-    public Member(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        Member[] members = new Member[N];
+        StringBuilder[] sb = new StringBuilder[201];
+        for (int i = 0; i < sb.length; i++) {
+            sb[i] = new StringBuilder();
+        }
 
         for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-            int age = Integer.parseInt(input[0]);
-            String name = input[1];
-            members[i] = new Member(age, name);
+            String[] str = br.readLine().split(" ");
+            int age = Integer.parseInt(str[0]);
+            String name = str[1];
+            sb[age].append(age).append(' ').append(name).append('\n');
         }
 
-        Arrays.sort(members, new Comparator<Member>() {
-            @Override
-            public int compare(Member m1, Member m2) {
-                if (m1.getAge() == m2.getAge()) {
-                    // 나이가 같으면 가입한 순서대로 정렬
-                    return 0;
-                } else {
-                    // 나이 오름차순 정렬
-                    return Integer.compare(m1.getAge(), m2.getAge());
-                }
-            }
-        });
-
-        for (Member member : members) {
-            System.out.println(member.getAge() + " " + member.getName());
+        StringBuilder fsb = new StringBuilder();
+        for (StringBuilder result : sb) {
+            fsb.append(result);
         }
+        System.out.println(fsb);
     }
 }
